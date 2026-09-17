46eac54652481fe37117ec68e7c160ef04b11af0
SPPGIWAPIKepuharjo <ifailmhprmdy@gmail.com>
2026-09-17 07:01:57 +0700

    Update script.js

--- script.js
+++ script.js
@@ -63,7 +63,7 @@ const dataMenu = {
         sayur: "Buncis Bawang Putih",
         buah: "Kelengkeng",
         tambahan: "-",
-        foto: "barcode (Konten Instagram (45)) - 5.png",
+        foto: "kamis.png",
         gizi: {
             energi: { kecil: "430,1 Kcal", besar: "520,4 Kcal", bumil: "624,2 Kcal", balita: "394,1 Kcal", alergen: "487,2 Kcal" },
             protein: { kecil: "17g", besar: "18,7g", bumil: "24,4g", balita: "16,4g", alergen: "23,5g" },
